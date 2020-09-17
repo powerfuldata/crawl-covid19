d4179732a6d46039c810864cc540b892a0b55dbf
吴超 <wuchao@zhongbaounion.com>
2020-09-17 16:40:12 +0800

    <爬虫初步>:页面数据爬取出来,后面使用node + ts重写

---
+++ src/app.js
@@ -0,0 +1,33 @@
+// import puppertter from 'puppeteer-core';
+const puppertter = require('puppeteer-core');
+const cheerio = require('cheerio')
+const init = async () => {
+  const browser = await puppertter.launch({
+    executablePath: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome',
+  })
+  const page = await browser.newPage();
+  await page.goto('https://apify.com/covid-19');
+  const html = await page.content();
+  const $ = cheerio.load(html)
+  const table = $('#actual-data').next('table');// 查找与标题紧邻的table元素
+  const thead = table.children().first();
+  const titles = thead.find('tr').children().map((i, el) => $(el).text()).get();
+  console.log('表头：',titles)
+
+  const tbody = table.children().last();
+  const dataSource = tbody.children().map((i,row) => {
+    return row.children.reduce((obj,column, index) => {
+      const text = $(column).text();
+      const key = titles[index];
+      if (key) {
+        obj[key] = text;
+      }
+      
+      return obj;
+    },{})
+  }).get()
+  console.log('dataSource:',dataSource)
+  browser.close();
+}
+
+init()
